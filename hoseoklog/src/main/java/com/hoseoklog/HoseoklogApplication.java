cef06c9d9439f608f445407012fbc7b8568888c9
HiiWee <wpdlzhf159@gmail.com>
2023-12-15 20:00:09 +0900

    init: project

---
+++ hoseoklog/src/main/java/com/hoseoklog/HoseoklogApplication.java
@@ -0,0 +1,13 @@
+package com.hoseoklog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoseoklogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoseoklogApplication.class, args);
+    }
+
+}
---
+++ hoseoklog/src/test/java/com/hoseoklog/HoseoklogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoseoklog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoseoklogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
